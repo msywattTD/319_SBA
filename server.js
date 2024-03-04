37adcd87f08d7a7c6b4a1a03cbfbc8b9dfabbf4b
Matt Wyatt <mswyatt.td@gmail.com>
2024-03-03 19:24:38 -0700

    added naked db connection

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//connect to db
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database."));
+//MIDDLEWARE
+
+//ROUTES
+// const userRoutes = require("./routes/users");
+// app.use("/users", userRouter);
+
+// const postsRoutes = require("./routes/posts");
+// app.use("/posts", postRoutes);
+
+// const commentsRoutes = require("./routes/comments");
+// app.use("/comments", commentRoutes);
+//ERRORS
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
